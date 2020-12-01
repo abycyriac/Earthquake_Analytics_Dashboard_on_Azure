2a456cbe3bb175f5500744d4e49111df74d6a4bd
abycyriac <abycyriac96@gmail.com>
2020-12-01 15:31:05 -0600

    V1.2

--- Earthquake_ Analytics_Dashboard_on_Azure/dbconnect.py
+++ Earthquake_ Analytics_Dashboard_on_Azure/dbconnect.py
@@ -1,9 +1,9 @@
 import pyodbc
 
-server = 'abycyriac96.database.windows.net'
-database = 'test'
-username = 'abycyriac96'
-password = 'Vadakkedath@2020'
+server = ''
+database = ''
+username = ''
+password = ''
 driver = '{ODBC Driver 17 for SQL Server}'
 try:
     connect = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
